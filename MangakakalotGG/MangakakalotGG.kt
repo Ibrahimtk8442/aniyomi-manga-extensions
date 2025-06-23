8cd0e8240236978ed8233f526fd78d6d52e16055
Ibrahimtk8442 <ibrahimtete8442@gmail.com>
2025-06-23 18:24:55 +0100

    Create MangakakalotGG.kt

---
+++ MangakakalotGG/MangakakalotGG.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.en.mangakakalotgg
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class MangakakalotGG : Madara(
+    "Mangakakalot.gg",
+    "https://mangakakalot.gg",
+    "en"
+)
